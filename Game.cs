f3a13e9c239532d0d97fc185b6390ab28e9e3d9e
victortpt <bustosoy@gmail.com>
2021-09-30 11:26:59 +0200

    first commit

---
+++ Game.cs
@@ -0,0 +1,79 @@
+using System;
+
+namespace Spaceman
+{
+    class Game {
+        private string codeword;
+        private string currentWord;
+        private int numberOfGuesses;
+        private int maxNumberOfGuesses;
+        private string[] guesses = new string[]{"space", "galaxy", "moon", "star", "planet", "sun", "asteroid", };
+        private Ufo ufo = new Ufo();
+
+        public string Codeword { get; set; }
+        public string CurrentWord { get; set; }
+        public int NumberOfGuesses { get; set; }
+        public int MaxNumberOfGuesses { get; set; }
+
+        public Game() {
+            Random random = new Random();
+            Codeword = guesses[random.Next(guesses.Length)];
+            MaxNumberOfGuesses = 3;
+            NumberOfGuesses = 0;
+            for (int i = 0; i < Codeword.Length; i++)
+            {
+                CurrentWord += '_';
+            }
+        }
+
+        public bool DidWin() {
+            return Codeword.Equals(CurrentWord);
+        }
+
+        public bool DidLose() {
+            return NumberOfGuesses >= MaxNumberOfGuesses;
+        }
+
+        public void Display() {
+            Ufo spaceship = new Ufo();
+            Console.WriteLine(spaceship.Stringify());
+
+            Console.WriteLine("Current word: "+CurrentWord);
+            Console.WriteLine("Number of guesses: "+NumberOfGuesses);
+        }
+
+        public void Ask() {
+            Console.WriteLine("Guess a letter: ");
+            string guess = Console.ReadLine();
+            if (guess.Length == 1) {
+                Console.WriteLine("You introduced: "+guess);
+                if (Codeword.Contains(guess)) {
+                    for (int i = 0; i < Codeword.Length; i++) {
+                        if (Codeword[i].Equals(guess[0])) {
+                            CurrentWord = CurrentWord.Remove(i, 1).Insert(i, guess.ToString());
+                            // The replace function exists in the string class but Codecademy says do it this way
+                        }
+                    }
+                } else {
+                    Console.WriteLine("Wrong guess! Now the human is closest to the alien ship :'(");
+                    numberOfGuesses++;
+                    ufo.AddPart();
+                }
+                
+            } else {
+                Console.WriteLine("Please enter a single letter.");
+                return ;
+            }
+        }
+
+        public void Greet() {
+            Console.WriteLine("================");
+            Console.WriteLine("Hello, Spaceman!");
+            Console.WriteLine("================");
+            Console.WriteLine("How to play: You need to save the human from alien abduction by guessing the letter");
+            Console.WriteLine("You have 3 lives, and you can guess wrong 3 times");
+            Console.WriteLine("Good luck!");
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Spaceman
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Game game = new Game();
+      game.Greet();
+      do {
+        game.Display();
+        game.Ask();
+      } while (!game.DidWin() || !game.DidLose());
+
+      Console.WriteLine("You {0}", game.DidWin() ? "win" : "lose");
+    }
+  }
+}
